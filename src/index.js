e1bff191113e4f708d7c7c3247d512293d41ac1e
max <zogreptile@gmail.com>
2021-04-21 23:25:16 +0700

    improvement - app init

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 import 'core-js/stable';
 import 'regenerator-runtime/runtime';
+import { Excel } from 'src/components/excel/Excel';
+
+import { Menu } from 'src/components/menu/Menu';
+import { Toolbar } from 'src/components/toolbar/Toolbar';
+import { Formula } from 'src/components/formula/Formula';
+import { Grid } from 'src/components/grid/Grid';
 
 import './styles/index.scss';
+
+const excel = new Excel('#root', {
+  components: [Menu, Toolbar, Formula, Grid],
+});
+
+excel.render();
